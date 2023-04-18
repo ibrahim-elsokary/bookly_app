c8282b52efa735f47e8ae53ab0219f0c28958e7f
ibrahim elsokary <107218787+ibrahimElsokkary@users.noreply.github.com>
2023-04-18 02:55:52 +0200

    add splash text animation

--- lib/features/Splash/presentation/views/screens/widgets/splash_view_body.dart
+++ lib/features/Splash/presentation/views/screens/widgets/splash_view_body.dart
@@ -1,11 +1,41 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/Splash/presentation/views/screens/widgets/sliding_text.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+
+     animationController.forward();    
+
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    //to prevent memory leak
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +43,13 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        SvgPicture.asset(AssetsData.logo,width: 300 ),
-        const SizedBox(height: 10,),
-        const Text("Read Free Books",textAlign: TextAlign.center,),
+        SvgPicture.asset(AssetsData.logo, width: 300),
+        const SizedBox(
+          height: 10,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
 }
+
