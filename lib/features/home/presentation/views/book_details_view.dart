97792bfa4b0b0b2d93a0fdc8310ff796855779cb
ibrahim elsokary <107218787+ibrahimElsokkary@users.noreply.github.com>
2023-04-19 02:27:37 +0200

    create book details

---
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,12 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view_body.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const  BookDetailsViewBody();
+  }
+}
