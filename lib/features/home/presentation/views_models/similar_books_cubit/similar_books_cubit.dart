0fa9603c2feabc6c5e631e49421a2a9c8c56006c
ibrahim elsokary <107218787+ibrahimElsokkary@users.noreply.github.com>
2023-04-20 22:55:59 +0200

    done with fetch similar books cubit

---
+++ lib/features/home/presentation/views_models/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/core/utils/error/failure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchSimilarBooks(String category) async {
+    emit(SimilarBooksLoading());
+    var result = await homeRepo.fetchSimilarBooks(category);
+    result.fold((failure) {
+      emit(
+        SimilarBooksFailure(failure.errorMsg),
+      );
+    }, (books) {
+      emit(
+        SimilarBooksSuccess(books),
+      );
+    });
+  }
+}
