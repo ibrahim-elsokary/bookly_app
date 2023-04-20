ca7240bddb55143731158f2a386bfc95762b8365
ibrahim elsokary <107218787+ibrahimElsokkary@users.noreply.github.com>
2023-04-20 06:17:00 +0200

    done with featured cubit

--- lib/features/home/presentation/views_models/featurd_books_cubit/featurd_book_cuibt_cubit.dart
+++ lib/features/home/presentation/views_models/featurd_books_cubit/featurd_book_cuibt_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/core/utils/error/failure.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featurd_book_cuibt_state.dart';
 
 class FeaturdBookCuibtCubit extends Cubit<FeaturdBookCuibtState> {
-  FeaturdBookCuibtCubit() : super(FeaturdBookCuibtInitial());
+  FeaturdBookCuibtCubit(this.homeRepo) : super(FeaturdBookCuibtInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturdBookCuibtLoading());
+    var result = await homeRepo.fetchFeatuerdBooks();
+    result.fold((failure) {
+      emit(FeaturdBookCuibtFailure(failure.errorMsg));
+    }, (books) {
+      emit(FeaturdBookCuibtSuccess(books));
+    });
+  }
 }
